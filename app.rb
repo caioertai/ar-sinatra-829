08c5182c4cbc8bbdf0b0455de6225be25b9955d2
Caio Andrade <andrade@hey.com>
2022-02-14 17:12:24 -0600

    Update gemfile

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
+# localhost:4567/ # Get me the home page
 get "/" do
   "Hello world!"
+end
+
+get "/le_wagon" do
+  "Hello Le Wagon!"
 end
