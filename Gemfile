ca5a90b8afccfe614b43bf1178b48ad54711737e
Caio Andrade <andrade@hey.com>
2022-02-14 18:19:24 -0600

    Create #show and #create actions on app.rb

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "faker"
 gem "pry"
+gem "pry-byebug"
 gem "rake"
 gem "sinatra"
 gem "sinatra-activerecord"
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative "config/application"
+require "pry-byebug"
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
@@ -8,8 +9,37 @@ get "/" do
   "Hello world!"
 end
 
-# http://localhost:4567/restaurants
+# GET /restaurants # lists restaurants
 get "/restaurants" do
-  # View... now print the template
+  # Ask MODEL for all restaurants
+  @restaurants = Restaurant.all
+  # Ask VIEW to display
   erb :"restaurants/index" # HTML embeded ruby
+end
+
+# GET /restaurants/1
+# GET /restaurants/10
+# GET /restaurants/5
+get "/restaurants/:id" do
+  # Ask MODEL for restaurant of the given id
+  @restaurant = Restaurant.find(params[:id])
+  # Ask restaurant for its name
+  erb :"restaurants/show" # details
+end
+
+post "/restaurants" do
+  # Coming from the form
+  # params => {
+  # "name"=>"Hangai",
+  # "address"=>"Portal da Ilha 123",
+  # "rating"=>"3"
+  # }
+  # Ask MODEL to instantiate a new restaurant
+  @restaurant = Restaurant.new(
+    name: params[:name],
+    address: params[:address],
+    rating: params[:rating]
+  )
+  @restaurant.save
+  redirect "/restaurants/#{@restaurant.id}"
 end
